eef18ce20ca191c5ea0f9cd1c0900a2b69c04cb5
Daniel Mascena <daniel.mascena@tripleplay.tv>
2022-04-26 17:44:08 +0100

    Restructuring the components directory
    modify the component structure to follow the atomic ui framework

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders TriplePlay UI Components', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/TriplePlay UI Components/i);
+  expect(headingElement).toBeInTheDocument();
 });
